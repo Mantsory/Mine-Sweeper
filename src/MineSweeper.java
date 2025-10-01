12848c14c025482d9a1cd0d888cccd31aa1ec54b
Mantsory <kolehmainenkaspian@gmail.com>
2025-10-01 19:54:41 -0400

    Initial Release version 0.1: -Added some prompts to create the code.

---
+++ src/MineSweeper.java
@@ -0,0 +1,16 @@
+/*
+ * Purpose: To play a simple terminal game of minesweeper.
+ *
+ * Author: Mantsory
+ * Version: 0.1
+ */
+
+public class MineSweeper {
+    //Send an initial welcome message
+    //generate the minefield
+    //prompt user to input a location to open
+    //open location and check for surrounding mines
+    //repeat
+    //end game messages
+    //prompt to play again
+}
